b63aff6cd589171e8fefecdf2106cfe219919e41
pryority <matthewapryor@gmail.com>
2023-12-03 11:57:52 -0500

    ✅ Complete Day 0 -- Submission Passed

--- src/day0/day0.spec.ts
+++ src/day0/day0.spec.ts
@@ -1,7 +1,9 @@
-import day0 from './index';
+import day0 from "./index";
+import fs from "fs";
+const ip1 = fs.readFileSync("inputs/day0/part1.txt", "utf-8");
 
-describe('On Day 0', () =>{
-    it(`part1 is identity function`, ()=>{
-        expect(day0.solveForPartOne('hello')).toBe('hello');
-    })
+describe("On Day 0", () => {
+  it(`part1 is identity function`, () => {
+    expect(day0.solveForPartOne(ip1)).toBe("55477");
+  });
 });
--- src/day0/index.ts
+++ src/day0/index.ts
@@ -1,18 +1,26 @@
 import { Day } from "../day";
 
 class Day0 extends Day {
+  constructor() {
+    super(0);
+  }
 
-    constructor(){
-        super(0);
-    }
+  solveForPartOne(input: string): string {
+    let sum: number = 0;
 
-    solveForPartOne(input: string): string {
-        return input;
-    }
+    input.split(/\r?\n/).map((line) => {
+      const lineArr = line.replace(/[^0-9]/g, "");
+      sum =
+        sum +
+        parseInt(lineArr.split("")[0] + lineArr.split("")[lineArr.length - 1]);
+    });
 
-    solveForPartTwo(input: string): string {
-        return input;
-    }
+    return sum.toString();
+  }
+
+  solveForPartTwo(input: string): string {
+    return input;
+  }
 }
 
-export default new Day0;
+export default new Day0();
